136080476d9b24c5d123cf988992f7ce515435eb
Nishant <nishantkumardash26@gmail.com>
2024-04-23 23:21:18 +0530

    signup

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,19 @@
-import prisma from "@repo/db/client";
-
 export default function Page(): JSX.Element {
   return (
     <main>
-      <div className="bg-slate-200">Hi There</div>
+      <div className="flex">
+        <div className="flex w-2/3 h-screen justify-center items-center">
+          <img
+            src="https://cdn.pixabay.com/photo/2015/12/07/10/42/chess-1080533_960_720.jpg"
+            className="w-full h-full"
+          ></img>
+        </div>
+        <div className="flex w-1/3 h-screen justify-center items-center bg-slate-600">
+          <h1 className="text-white text-amber-300 text-2xl mx-4 sm:m-4 md:m-4">
+            Play Chess Like Never Before
+          </h1>
+        </div>
+      </div>
     </main>
   );
 }
